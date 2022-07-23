06e07faec11a7cee33c4056472778c6fd2b42115
Wayne Barrass-Brown <waynebb7@gmail.com>
2022-07-23 10:37:52 +0100

    Hello bug-a

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world updated another bug-a!")
+        Text("Hello, bug-a!")
             .padding()
     }
 }
