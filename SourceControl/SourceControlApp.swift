364b88650c2c394b246b89b0fe170cb052aad721
Wayne Barrass-Brown <waynebb7@gmail.com>
2022-07-23 10:03:50 +0100

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Wayne Barrass-Brown on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
